4f8b2740da9274ed860fcf077868b406ae0a0fd3
Armando Hernandez de la Vega <0219796@gmail.up.edu.mx>
2020-07-28 21:25:31 -0500

    Adding Project List

--- index.js
+++ index.js
@@ -101,4 +101,6 @@ window.onload = function() {
 const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
 const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
 
-console.log(vw, vh)
+if(vw < 700){
+    document.getElementById('gif').src = "./Assets/Coding(Móvil).gif"
+}
